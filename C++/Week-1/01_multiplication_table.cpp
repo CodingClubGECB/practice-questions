c0c161113ebf1299de9cbfb7916aa8ecf6db5d01
Akshaylal <akshaylals2000@gmail.com>
2020-03-19 12:49:59 +0530

    table

--- C++/Week-1/01_multiplication_table.cpp
+++ C++/Week-1/01_multiplication_table.cpp
@@ -1,3 +1,5 @@
+//1.Write a program to print the multiplication table of a number upto 10. 
+
 #include <iostream>
 
 using namespace std;
@@ -7,4 +9,5 @@ int main(){
 	cin>>n;
 	for(int i = 0; i <=10; ++i)
 		cout<<n<<" x "<<i<<" = "<<n*i<<endl;
+	return 0;
 }
